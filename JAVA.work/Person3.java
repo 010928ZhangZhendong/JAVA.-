cf42b73a8842d4099286c870ffc591203c37b198
ZZD01 <71304192+ZZD01@users.noreply.github.com>
2020-09-21 23:06:18 +0800

    Rename Person.java to Person3.java

--- JAVA.work/Person3.java
+++ JAVA.work/Person3.java
@@ -3,7 +3,7 @@ import java.awt.*;
 /**
  * A person that can be manipulated and that draws itself on a canvas.
  * 
- * @author  Michael Kšlling and David J. Barnes
+ * @author  Michael KÅ¡lling and David J. Barnes
  * @version 2016.02.29
  */
 
